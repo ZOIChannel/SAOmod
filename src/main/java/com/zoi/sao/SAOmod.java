8d525ba265d87ccd0ab1292cbe0e4a1057031bf6
川添晴翔 <toritoribox@gmail.com>
2019-06-01 21:36:16 +0900

    "black_ingot"追加 精錬レシピ追加 状態:実行可能(A) ("Examplemod"が残らないようにした)
    Signed-off-by: 川添晴翔 <toritoribox@gmail.com>

--- src/main/java/com/zoi/sao/SAOmod.java
+++ src/main/java/com/zoi/sao/SAOmod.java
@@ -6,6 +6,21 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.IFuelHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+
 
 
 
@@ -13,6 +28,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class SAOmod {
 
     public static Item steel_ingot;
+    public static Item black_ingot;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -21,5 +37,17 @@ public class SAOmod {
                 .setUnlocalizedName("steel ingot")
                 .setTextureName("SAOmod:steel ingot");
         GameRegistry.registerItem(steel_ingot, "steel ingot");
+
+        black_ingot = new Item()
+                .setCreativeTab(CreativeTabs.tabMisc)
+                .setUnlocalizedName("black ingot")
+                .setTextureName("SAOmod:black ingot");
+        GameRegistry.registerItem(black_ingot, "black ingot");
+    }
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        GameRegistry.addSmelting(Items.iron_ingot, new ItemStack(SAOmod.steel_ingot), 0.8F);
+        GameRegistry.addSmelting(SAOmod.steel_ingot, new ItemStack(SAOmod.black_ingot), 0.8F);
+
     }
 }
