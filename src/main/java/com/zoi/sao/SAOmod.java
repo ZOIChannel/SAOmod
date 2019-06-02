425dc9fd233d1eb361b02184dd7d9d883688a6c2
ZOI <zoizoichannel@gmail.com>
2019-06-02 22:18:37 +0900

    インゴット、ハンドル類の追加 alpha_0.0.2 状態:実行可能(A)

--- src/main/java/com/zoi/sao/SAOmod.java
+++ src/main/java/com/zoi/sao/SAOmod.java
@@ -6,29 +6,23 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import cpw.mods.fml.common.IFuelHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
 
 
 
 
 
-@Mod(modid = "SAOmod", name = "SAOmod", version = "alpha_0.0.1")
+@Mod(modid = "SAOmod", name = "SAOmod", version = "alpha_0.0.2")
 public class SAOmod {
 
     public static Item steel_ingot;
     public static Item black_ingot;
+    public static Item crystallite_ingot;
+    public static Item steel_handle;
+    public static Item black_handle;
+    public static Item crystallite_handle;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -43,11 +37,42 @@ public class SAOmod {
                 .setUnlocalizedName("black ingot")
                 .setTextureName("SAOmod:black_ingot");
         GameRegistry.registerItem(black_ingot, "black ingot");
+
+        crystallite_ingot = new Item()
+                .setCreativeTab(CreativeTabs.tabMisc)
+                .setUnlocalizedName("crystallite ingot")
+                .setTextureName("SAOmod:crystallite_ingot");
+        GameRegistry.registerItem(crystallite_ingot, "crystallite ingot");
+
+        steel_handle = new Item()
+                .setCreativeTab(CreativeTabs.tabMisc)
+                .setUnlocalizedName("steel handle")
+                .setTextureName("SAOmod:steel_handle");
+        GameRegistry.registerItem(steel_handle, "steel handle");
+
+        black_handle = new Item()
+                .setCreativeTab(CreativeTabs.tabMisc)
+                .setUnlocalizedName("black handle")
+                .setTextureName("SAOmod:black_handle");
+        GameRegistry.registerItem(black_handle, "black handle");
+
+        crystallite_handle = new Item()
+                .setCreativeTab(CreativeTabs.tabMisc)
+                .setUnlocalizedName("crystallite handle")
+                .setTextureName("SAOmod:crystallite_handle");
+        GameRegistry.registerItem(crystallite_handle, "crystallite handle");
+
+
+
     }
     @EventHandler
     public void init(FMLInitializationEvent event) {
         GameRegistry.addSmelting(Items.iron_ingot, new ItemStack(SAOmod.steel_ingot), 0.8F);
         GameRegistry.addSmelting(SAOmod.steel_ingot, new ItemStack(SAOmod.black_ingot), 0.8F);
+        GameRegistry.addRecipe(new ItemStack(SAOmod.crystallite_ingot), "YXY", "XZX", "YXY", 'X', Items.gold_ingot, 'Y', Items.iron_ingot, 'Z', Items.diamond);
+        GameRegistry.addRecipe(new ItemStack(SAOmod.steel_handle), "  X", " X ", "X  ", 'X', SAOmod.steel_ingot);
+        GameRegistry.addRecipe(new ItemStack(SAOmod.black_handle), "  X", " X ", "X  ", 'X', SAOmod.black_ingot);
+        GameRegistry.addRecipe(new ItemStack(SAOmod.crystallite_handle), "  X", " X ", "X  ", 'X', SAOmod.crystallite_ingot);
 
     }
 }
